966baf868d4eb8b48e3730afdce7d8925cfe50bb
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2024-05-26 19:36:52 -0300

    Create assembly7.asm

---
+++ arq_aulas/assembly7.asm
@@ -0,0 +1,6 @@
+; Exercicio 1. Implementar um código em linguagem de montagem ('assembly') para o montador ('assembler') do microcontrolador Atmega2560 que:
+; a. Armazene inicialmente os 20 impares acima de 100 nos endereços de memória acima de 0x200, 
+; construindo uma tabela de dados. 
+; b. Em seguida, leia sequencialemnte cada um dos números armazenados na tabela, 
+; armazenando também em sequência o par imediatamente inferior ao número lido em uma nova tabela iniciada a contar do endereço após a última linha da tabela anterior.
+; c. Aramazenar, por último: o endereço inicial da primeira tabela; e o tamanho (número de linhas) de cada tabela.
